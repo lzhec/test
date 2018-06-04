ce807253324f239393689118ad1bc886c9e2f5d6
lzhec <nikita090587@gmail.com>
2018-06-04 19:26:54 +0800

    .

---
+++ quickstart-master/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AppService } from './app.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+})
+
+export class AppComponent implements OnInit  {
+  constructor(private appService: AppService) {}
+
+  pricelist: any;
+
+  ngOnInit() {
+    this.appService.getPriceList().subscribe(d => {
+      this.pricelist = d.pricelist;
+    });
+  }
+
+
+}

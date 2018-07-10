a5f998b1c719452170f07294a65d4d0a60f2e37d
lzhec <nikita090587@gmail.com>
2018-07-10 22:43:00 +0800

    .

--- autorisation/app/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-for($i = 0; $i < count($user); ++$i){
-    echo "<tr='?id=".$user[$i]['id']."'><td>".$user[$i]['name']."</td><td>".$user[$i]['email']."</td><td>".$user[$i]['created_at']."</td>;
-    echo "</tr>"";
-}
-
--- autorisation/public/js/popupsort.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	
-	// всплывающее меню сортировки
-	
-	$('.select-sort').click(function(){	
-		$('.sortlist').slideToggle(200);
-	});
-});
